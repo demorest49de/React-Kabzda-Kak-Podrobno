c82c491ae2bed12f11072ddeda3b9674350e96b4
Shevchenko Andrey <demorest49de@gmail.com>
2024-03-21 18:56:59 +0300

    Changes ReactMemo.stories.tsx

---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,38 @@
+import React, {useState} from "react"
+
+export default {
+    title: 'React.memo demo',
+}
+
+const MessageCounter = (props: { count: number }) => {
+    return <div>{props.count}</div>
+}
+
+const SecretUsers = (props: { users: Array<string> }) => {
+    console.log(' render users: ');
+    return <div>
+        {props.users.map((u, i) => <div key={i}>{u}</div>)}
+    </div>
+}
+
+const Users = React.memo(SecretUsers)
+
+export const ReactMemo = () => {
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(['dimich', 'valera', 'kayatja', 'natasha'])
+
+    const addUser = (value:  string) =>{
+        setUsers([...users, 'Sveta ' + new Date().getTime()])
+    }
+
+    return <>
+        <button onClick={() => {
+            setCounter(counter + 1)
+        }}> +
+        </button>
+        <button onClick={addUser}> add user
+        </button>
+        <MessageCounter count={counter}/>
+        <Users users={users}/>
+    </>
+}
